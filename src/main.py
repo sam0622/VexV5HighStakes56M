18283fc39e0490097dc1e4497b522c9a6bf1d76f
sam0622 <sgreenfield517@gmail.com>
2024-12-11 15:01:31 -0500

    Added #regions

--- src/main.py
+++ src/main.py
@@ -7,8 +7,11 @@
 #                                                                              #
 # ---------------------------------------------------------------------------- #
 
+# region Configuration
+
 # Library imports
 from vex import *
+from os import name
 
 # Brain + Controller definition
 brain: Brain = Brain()
@@ -30,42 +33,47 @@ intake.set_velocity(100, PERCENT)
 # Pneumatics configuration
 goal_lock: DigitalOut = DigitalOut(brain.three_wire_port.a)
 goal_release: DigitalOut = DigitalOut(brain.three_wire_port.b)
+# endregion
 
-#-----------------#
-# Auton functions #
-#-----------------#
+# region Auton Functions
 
-def move_forward(time: float, speed: int=100) -> None:
+
+def move_forward(time: float, speed: int = 100) -> None:
     drive_motors_right.spin(FORWARD, speed, PERCENT)
     drive_motors_left.spin(FORWARD, speed, PERCENT)
     sleep(time)
     drive_motors_right.stop()
     drive_motors_left.stop()
 
-def move_backward(time: float, speed: int=100) -> None:
+
+def move_backward(time: float, speed: int = 100) -> None:
     drive_motors_right.spin(REVERSE, speed, PERCENT)
     drive_motors_left.spin(REVERSE, speed, PERCENT)
     sleep(time)
     drive_motors_right.stop()
     drive_motors_left.stop()
 
-def turn_right(time: float, speed: int=100) -> None:
+
+def turn_right(time: float, speed: int = 100) -> None:
     drive_motors_right.spin(REVERSE, speed, PERCENT)
     drive_motors_left.spin(FORWARD, speed, PERCENT)
     sleep(time)
     drive_motors_right.stop()
     drive_motors_left.stop()
 
-def turn_left(time: float, speed: int=100) -> None:
+
+def turn_left(time: float, speed: int = 100) -> None:
     drive_motors_right.spin(FORWARD, speed, PERCENT)
     drive_motors_left.spin(REVERSE, speed, PERCENT)
     sleep(time)
     drive_motors_right.stop()
     drive_motors_left.stop()
 
-#------------------#
-# Button callbacks #
-#------------------#
+
+# endregion
+
+# region Callbacks
+
 
 def controller_R1_pressed() -> None:
     conveyor.spin(FORWARD)
@@ -73,11 +81,13 @@ def controller_R1_pressed() -> None:
         wait(5, MSEC)
     conveyor.stop()
 
+
 def controller_L1_pressed() -> None:
     conveyor.spin(REVERSE)
     while controller_1.buttonR1.pressing():
         wait(5, MSEC)
     conveyor.stop()
+
 
 def controller_R2_pressed() -> None:
     intake.spin(FORWARD)
@@ -85,11 +95,13 @@ def controller_R2_pressed() -> None:
         wait(5, MSEC)
     intake.stop()
 
+
 def controller_L2_pressed() -> None:
     intake.spin(REVERSE)
     while controller_1.buttonL2.pressing():
         wait(5, MSEC)
     intake.stop()
+
 
 def controller_A_pressed() -> None:
     if goal_lock.value() == 0:
@@ -106,9 +118,13 @@ controller_1.buttonR2.pressed(controller_R2_pressed)
 controller_1.buttonL2.pressed(controller_L2_pressed)
 controller_1.buttonA.pressed(controller_A_pressed)
 wait(15, MSEC)
+# endregion
 
-# Main driving thread
+# region Control
+
 intake.set_velocity(100, PERCENT)
+
+
 def drive_task() -> None:
     while True:
 
@@ -118,14 +134,20 @@ def drive_task() -> None:
 
         drive_motors_right.set_velocity(controller_1.axis2.position(), PERCENT)
         drive_motors_left.set_velocity(controller_1.axis3.position(), PERCENT)
-        
+
         wait(5, MSEC)
+
 
 def pre_autonomous() -> None:
     pass
 
+
 def autonomous() -> None:
     pass
 
+
 def user_control() -> None:
     drive_thread: Thread = Thread(drive_task)
+
+
+# endregion
