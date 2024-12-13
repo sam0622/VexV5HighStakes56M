ee7776c1dd4b3c169255c1e1d95baf57bad7206b
sam0622 <sgreenfield517@gmail.com>
2024-12-13 15:09:39 -0500

    offence auton

--- src/main.py
+++ src/main.py
@@ -28,33 +28,26 @@ intake: Motor = Motor(Ports.PORT9, True)
 
 drive_motors_right: MotorGroup = MotorGroup(drive_fr, drive_mr, drive_br)
 drive_motors_left: MotorGroup = MotorGroup(drive_fl, drive_ml, drive_bl)
-conveyor.set_velocity(100, PERCENT)
-intake.set_velocity(100, PERCENT)
 
 # Pneumatics configuration
 goal_solenoid: DigitalOut = DigitalOut(brain.three_wire_port.a)
-goal_solenoid.set(1)
-# Auton conditions
-is_defense: bool = True
-is_auton: bool = True
-is_pre_auton: bool = True
 # endregion
 
 # region Auton Functions
 
 
 def move_forward(time: float, speed: int = 100) -> None:
-    drive_motors_right.spin(FORWARD, speed, PERCENT)
-    drive_motors_left.spin(FORWARD, speed, PERCENT)
-    sleep(time)
+    drive_motors_right.spin(REVERSE, speed, PERCENT)
+    drive_motors_left.spin(REVERSE, speed, PERCENT)
+    sleep(time, SECONDS)
     drive_motors_right.stop()
     drive_motors_left.stop()
 
 
 def move_backward(time: float, speed: int = 100) -> None:
-    drive_motors_right.spin(REVERSE, speed, PERCENT)
-    drive_motors_left.spin(REVERSE, speed, PERCENT)
-    sleep(time)
+    drive_motors_right.spin(FORWARD, speed, PERCENT)
+    drive_motors_left.spin(FORWARD, speed, PERCENT)
+    sleep(time, SECONDS)
     drive_motors_right.stop()
     drive_motors_left.stop()
 
@@ -62,7 +55,7 @@ def move_backward(time: float, speed: int = 100) -> None:
 def turn_right(time: float, speed: int = 100) -> None:
     drive_motors_right.spin(REVERSE, speed, PERCENT)
     drive_motors_left.spin(FORWARD, speed, PERCENT)
-    sleep(time)
+    sleep(time, SECONDS)
     drive_motors_right.stop()
     drive_motors_left.stop()
 
@@ -70,9 +63,36 @@ def turn_right(time: float, speed: int = 100) -> None:
 def turn_left(time: float, speed: int = 100) -> None:
     drive_motors_right.spin(FORWARD, speed, PERCENT)
     drive_motors_left.spin(REVERSE, speed, PERCENT)
-    sleep(time)
+    sleep(time, SECONDS)
     drive_motors_right.stop()
     drive_motors_left.stop()
+
+
+def intake_and_conveyer(time: float) -> None:
+    intake.spin(FORWARD)
+    conveyor.spin(FORWARD)
+    sleep(time, SECONDS)
+    intake.stop()
+    conveyor.stop()
+
+
+def grab(time: float) -> None:
+    intake.spin(FORWARD)
+    sleep(time, SECONDS)
+    intake.stop()
+
+
+def convey(time: float) -> None:
+    conveyor.spin(FORWARD)
+    sleep(time, SECONDS)
+    conveyor.stop()
+
+
+def switch_goal_lock() -> None:
+    if goal_solenoid.value() == 0:
+        goal_solenoid.set(True)
+    else:
+        goal_solenoid.set(False)
 
 
 # endregion
@@ -112,49 +132,20 @@ def controller_A_pressed() -> None:
     switch_goal_lock()
 
 
-def controller_B_pressed() -> None:
-    global is_auton, is_pre_auton, is_defense
-    if is_auton and is_pre_auton:
-        controller_1.rumble(".--")
-        if is_defense:
-            is_defense = False
-            controller_1.screen.clear_screen()
-            controller_1.screen.print("Offense")
-        else:
-            controller_1.screen.clear_screen()
-            controller_1.screen.print("Defense")
-            is_defense = True
-    else:
-        controller_1.rumble(".")
-
-
 controller_1.buttonR1.pressed(controller_R1_pressed)
 controller_1.buttonL1.pressed(controller_L1_pressed)
 controller_1.buttonR2.pressed(controller_R2_pressed)
 controller_1.buttonL2.pressed(controller_L2_pressed)
 controller_1.buttonA.pressed(controller_A_pressed)
-controller_1.buttonB.pressed(controller_B_pressed)
 
 wait(15, MSEC)
 # endregion
 
 # region Control
 
-intake.set_velocity(100, PERCENT)
-
-
-def switch_goal_lock() -> None:
-    if goal_solenoid.value() == 0:
-        goal_solenoid.set(True)
-    else:
-        goal_solenoid.set(False)
-
 
 def drive_task() -> None:
-    global is_auton, is_pre_auton
-    while True:
-        is_auton, is_pre_auton = False, False
-        
+    while True:        
         # Motor control
         drive_motors_right.spin(FORWARD)
         drive_motors_left.spin(FORWARD)
@@ -165,25 +156,20 @@ def drive_task() -> None:
 
         wait(5, MSEC)
 
-
 def pre_autonomous() -> None:
-    global is_auton, is_pre_auton
-    is_auton, is_pre_auton  = True, True
-    controller_1.screen.print("Defense")
-    pass
+    conveyor.set_velocity(100, PERCENT)
+    intake.set_velocity(100, PERCENT)
+    goal_solenoid.set(0)
 
 
 def autonomous() -> None:
-    global is_auton, is_defense
-    is_auton, is_pre_auton = True, False
-    if is_defense:
-        pass
-    else:
-        pass
+    move_forward(0.5)
+    intake_and_conveyer(1.3)
+    turn_right(0.9)
+    move_backward(1.4)
 
 
 def user_control() -> None:
-    is_auton, is_pre_auton = False, False
     drive_task()
 
 
